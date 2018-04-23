4ac141663ef4b027e4290ee6fdf9609c947c2a26
César Pomar <cesarpomar18@gmail.com>
2018-04-23 23:34:23 +0200

    storage functions

--- ignis/rpc/executor/IReducerModule.thrift
+++ ignis/rpc/executor/IReducerModule.thrift
@@ -7,9 +7,9 @@ include "../IRemoteException.thrift"
 include "IFunction.thrift"
 
 service IReducerModule{
-	list<string> getKeys() throws (1:IRemoteException.IRemoteException ex),
+	list<i64> getKeys(1: IFunction.IFunction funct) throws (1:IRemoteException.IRemoteException ex),
 	void setExecutorKeys(1: string host, 2: i32 port, 3: list<i64> keys_id, 4: i64 msg_id) throws (1:IRemoteException.IRemoteException ex),
 	void joinData(1: list<i64> msg_ids) throws (1:IRemoteException.IRemoteException ex),
-	void reduceByKey(3: IFunction.IFunction funct) throws (1:IRemoteException.IRemoteException ex),
+	void reduceByKey() throws (1:IRemoteException.IRemoteException ex),
 	void reset() throws (1:IRemoteException.IRemoteException ex);
 }
--- ignis/rpc/executor/IStorageModule.thrift
+++ ignis/rpc/executor/IStorageModule.thrift
@@ -2,6 +2,11 @@ namespace cpp ignis.rpc.executor
 namespace java org.ignis.rpc.executor
 namespace py ignis.rpc.executor.storage
 
-service IStorageModule{
 
+include "../IRemoteException.thrift"
+
+service IStorageModule{
+	void cache(1: i64 id, 2: string storage) throws (1:IRemoteException.IRemoteException ex),
+	void uncache(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
+	void load(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
 }
