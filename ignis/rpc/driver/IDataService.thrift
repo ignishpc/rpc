9061fe5258b039b23f6ec579f6da2045995989df
César Pomar <cesarpomar18@gmail.com>
2018-07-04 04:39:55 +0200

    file service and new functions

--- ignis/rpc/driver/IDataService.thrift
+++ ignis/rpc/driver/IDataService.thrift
@@ -15,7 +15,7 @@ struct IDataId{
 service IDataService{
 	void keep(1: IDataId data, 2: i8 level) throws (1:IRemoteException.IRemoteException ex),
 	IDataId _map(1: IDataId data, 2: IFunction.IFunction _function) throws (1:IRemoteException.IRemoteException ex),
-	IDataId streamingMap(1: IDataId data, 2: IFunction.IFunction _function) throws (1:IRemoteException.IRemoteException ex),
+	IDataId streamingMap(1: IDataId data, 2: IFunction.IFunction _function, 3: bool ordered) throws (1:IRemoteException.IRemoteException ex),
 	IDataId reduceByKey(1: IDataId data, 2: IFunction.IFunction _function) throws (1:IRemoteException.IRemoteException ex),
 	void saveAsFile(1: IDataId data, 2: string path, 3: bool join) throws (1:IRemoteException.IRemoteException ex);
 }
