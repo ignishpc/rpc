1a50fbcc9f9525a19b43f8a0034913acc0fc3016
César Pomar <cesarpomar18@gmail.com>
2018-08-10 07:39:06 +0200

    new unified address

--- ignis/rpc/executor/IKeysModule.thrift
+++ ignis/rpc/executor/IKeysModule.thrift
@@ -9,7 +9,7 @@ include "../ISourceFunction.thrift"
 
 service IKeysModule{
 	map cpp_type "std::unordered_map<int64_t, int64_t>"  <i64, i64> getKeys(1: bool single) throws (1:IRemoteException.IRemoteException ex),
-	void sendPairs(1: string host, 2: i32 port, 3: list<i64> keys_id) throws (1:IRemoteException.IRemoteException ex),
+	void sendPairs(1: string addr, 2: list<i64> keys_id) throws (1:IRemoteException.IRemoteException ex),
 	void joinPairs() throws (1:IRemoteException.IRemoteException ex),
 	void reset() throws (1:IRemoteException.IRemoteException ex);
 }
