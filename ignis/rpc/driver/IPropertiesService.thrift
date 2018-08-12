ec4a9242e312ab8ee10f9c4d09ce67869697f0a7
César Pomar <cesarpomar18@gmail.com>
2018-08-12 02:00:10 +0200

    property copy

--- ignis/rpc/driver/IPropertiesService.thrift
+++ ignis/rpc/driver/IPropertiesService.thrift
@@ -6,13 +6,14 @@ namespace py ignis.rpc.driver.properties
 include "../IRemoteException.thrift"
 
 service IPropertiesService{
-	i64 newInstance();
-	string setProperty(1: i64 id, 2: string key, 3: string value)throws (1:IRemoteException.IRemoteException ex),
-	string getProperty(1: i64 id, 2: string key)throws (1:IRemoteException.IRemoteException ex),
-	bool isProperty(1: i64 id, 2: string key)throws (1:IRemoteException.IRemoteException ex),
-	map<string, string> toMap(1: i64 id)throws (1:IRemoteException.IRemoteException ex),
-	void fromMap(1: i64 id, 2:map<string, string> _map)throws (1:IRemoteException.IRemoteException ex),
-	void toFile(1: i64 id, 2: string path)throws (1:IRemoteException.IRemoteException ex),
-	void fromFile(1: i64 id, 2: string path)throws (1:IRemoteException.IRemoteException ex);
-	void reset(1: i64 id)throws (1:IRemoteException.IRemoteException ex);
+	i64 newInstance() throws (1:IRemoteException.IRemoteException ex),
+	i64 newInstance2(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
+	string setProperty(1: i64 id, 2: string key, 3: string value) throws (1:IRemoteException.IRemoteException ex),
+	string getProperty(1: i64 id, 2: string key) throws (1:IRemoteException.IRemoteException ex),
+	bool isProperty(1: i64 id, 2: string key) throws (1:IRemoteException.IRemoteException ex),
+	map<string, string> toMap(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
+	void fromMap(1: i64 id, 2:map<string, string> _map) throws (1:IRemoteException.IRemoteException ex),
+	void toFile(1: i64 id, 2: string path) throws (1:IRemoteException.IRemoteException ex),
+	void fromFile(1: i64 id, 2: string path) throws (1:IRemoteException.IRemoteException ex);
+	void reset(1: i64 id) throws (1:IRemoteException.IRemoteException ex);
 }
