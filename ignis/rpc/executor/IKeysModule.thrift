1fc49daa7f8d3d145ab7f06bd9cb2827f80a8ece
César Pomar <cesarpomar18@gmail.com>
2018-10-24 19:07:24 +0200

    reduceByKey and test

--- ignis/rpc/executor/IKeysModule.thrift
+++ ignis/rpc/executor/IKeysModule.thrift
@@ -16,6 +16,7 @@ struct IExecutorKeys{
 service IKeysModule{
 	list<i64> getKeys() throws (1:IRemoteException.IRemoteException ex),
 	map cpp_type "std::unordered_map<int64_t, int64_t>"  <i64, i64> getKeysWithCount() throws (1:IRemoteException.IRemoteException ex),
-	void prepareKeys(1: list<IExecutorKeys> executorKeys) throws (1:IRemoteException.IRemoteException ex);
+	void prepareKeys(1: list<IExecutorKeys> executorKeys) throws (1:IRemoteException.IRemoteException ex),
+	void collectKeys() throws (1:IRemoteException.IRemoteException ex),
 	void reduceByKey(1: ISource.ISource funct) throws (1:IRemoteException.IRemoteException ex);
 }
