1e10625e895c4cf855b53759ff790ec734b8a081
César Pomar <cesarpomar18@gmail.com>
2018-12-05 02:12:03 +0100

    new api methods, context and cache

--- ignis/rpc/driver/IDataService.thrift
+++ ignis/rpc/driver/IDataService.thrift
@@ -13,7 +13,6 @@ struct IDataId{
 }
 
 service IDataService{
-	void keep(1: IDataId data, 2: i8 level) throws (1:IRemoteException.IRemoteException ex),
 	void setName(1: IDataId data, 2: string name) throws (1:IRemoteException.IRemoteException ex),
 	IDataId _map(1: IDataId data, 2: ISource.ISource _function) throws (1:IRemoteException.IRemoteException ex),
 	IDataId flatmap(1: IDataId data, 2: ISource.ISource _function) throws (1:IRemoteException.IRemoteException ex),
@@ -22,7 +21,14 @@ service IDataService{
 	IDataId streamingFlatmap(1: IDataId data, 2: ISource.ISource _function, 3: bool ordered) throws (1:IRemoteException.IRemoteException ex),
 	IDataId streamingFilter(1: IDataId data, 2: ISource.ISource _function, 3: bool ordered) throws (1:IRemoteException.IRemoteException ex),
 	IDataId reduceByKey(1: IDataId data, 2: ISource.ISource _function) throws (1:IRemoteException.IRemoteException ex),
+	IDataId values(1: IDataId data) throws (1:IRemoteException.IRemoteException ex),
 	IDataId shuffle(1: IDataId data) throws (1:IRemoteException.IRemoteException ex),
+	IDataId parallelize() throws (1:IRemoteException.IRemoteException ex),
+	binary take(1: IDataId data, 2: i64 n, 3: bool light) throws (1:IRemoteException.IRemoteException ex),
+	binary takeSample(1: IDataId data, 2: i64 n, 3: bool withRemplacement, 4: i32 seed, 5: bool randomSeed, 6: bool light) throws (1:IRemoteException.IRemoteException ex),
+	binary collect(1: IDataId data, 2: bool light) throws (1:IRemoteException.IRemoteException ex),
 	void saveAsTextFile(1: IDataId data, 2: string path, 3: bool join) throws (1:IRemoteException.IRemoteException ex),
-	void saveAsJsonFile(1: IDataId data, 2: string path, 3: bool join) throws (1:IRemoteException.IRemoteException ex);
+	void saveAsJsonFile(1: IDataId data, 2: string path, 3: bool join) throws (1:IRemoteException.IRemoteException ex),
+	void cache(1: IDataId data)  throws (1:IRemoteException.IRemoteException ex);
+	void uncache(1: IDataId data)  throws (1:IRemoteException.IRemoteException ex);
 }
--- ignis/rpc/executor/IStorageModule.thrift
+++ ignis/rpc/executor/IStorageModule.thrift
@@ -7,9 +7,9 @@ include "../IRemoteException.thrift"
 
 service IStorageModule{
 	i64 count() throws (1:IRemoteException.IRemoteException ex),
-	void cache(1: i64 id, 2: string storage) throws (1:IRemoteException.IRemoteException ex),
+	void cache(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
 	void uncache(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
-	void restore(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
+	void loadCache(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
 	void saveContext(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
 	void loadContext(1: i64 id) throws (1:IRemoteException.IRemoteException ex);
 }
