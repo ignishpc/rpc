ace4042eb1aa4d928c6123d607ade9ad9c549ed9
César Pomar <cesarpomar18@gmail.com>
2018-12-30 07:26:47 +0100

    sort methods

--- ignis/rpc/executor/ISortModule.thrift
+++ ignis/rpc/executor/ISortModule.thrift
@@ -2,6 +2,11 @@ namespace cpp ignis.rpc.executor
 namespace java org.ignis.rpc.executor
 namespace py ignis.rpc.executor.sort
 
-service ISortModule{
 
+include "../IRemoteException.thrift"
+include "../ISource.thrift"
+
+service ISortModule{
+	void localCustomSort(1: ISource.ISource funct, 2: bool ascending) throws (1:IRemoteException.IRemoteException ex),
+	void localSort(1: bool ascending) throws (1:IRemoteException.IRemoteException ex);
 }
