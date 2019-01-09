ea410e18e46e2f517ff8e30bcf42496478a8456a
César Pomar <cesarpomar18@gmail.com>
2019-01-09 22:25:55 +0100

    new map functions

--- ignis/rpc/executor/IMapperModule.thrift
+++ ignis/rpc/executor/IMapperModule.thrift
@@ -11,6 +11,8 @@ service IMapperModule{
 	void flatmap(1: ISource.ISource funct) throws (1:IRemoteException.IRemoteException ex),
 	void filter(1: ISource.ISource funct) throws (1:IRemoteException.IRemoteException ex),
 	void keyBy(1: ISource.ISource funct) throws (1:IRemoteException.IRemoteException ex),
+	void mapPartition(1: ISource.ISource funct) throws (1:IRemoteException.IRemoteException ex),
+	void mapPartitionWithIndex(1: i64 idx, 2: ISource.ISource funct) throws (1:IRemoteException.IRemoteException ex),
 	void values() throws (1:IRemoteException.IRemoteException ex),
 	void streamingMap(1: ISource.ISource funct, 2: bool ordered) throws (1:IRemoteException.IRemoteException ex),
 	void streamingFlatmap(1: ISource.ISource funct, 2: bool ordered) throws (1:IRemoteException.IRemoteException ex),
