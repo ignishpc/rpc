9c749436323a3ff2ad6f8afea72f8027b3cfb489
César Pomar <cesarpomar18@gmail.com>
2019-01-11 00:50:38 +0100

    removeContext

--- ignis/rpc/executor/IStorageModule.thrift
+++ ignis/rpc/executor/IStorageModule.thrift
@@ -12,6 +12,7 @@ service IStorageModule{
 	void loadCache(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
 	void saveContext(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
 	void loadContext(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
+	void removeContext(1: i64 id) throws (1:IRemoteException.IRemoteException ex),
 	binary take(1: i64 msg_id, 2: string addr, 3: i64 n, 4: bool light) throws (1:IRemoteException.IRemoteException ex),
 	binary takeSample(1: i64 msg_id, 2: string addr, 3: i64 n, 4: bool withRemplacement, 5: i32 seed, 6: bool light) throws (1:IRemoteException.IRemoteException ex),
 	binary collect(1: i64 msg_id, 2: string addr, 3: bool light) throws (1:IRemoteException.IRemoteException ex);
