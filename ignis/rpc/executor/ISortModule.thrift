c75195fc1208b5edf7a0f3e9272b63cfdf560260
César Pomar <cesarpomar18@gmail.com>
2019-01-10 20:38:38 +0100

    executor sort functions

--- ignis/rpc/executor/ISortModule.thrift
+++ ignis/rpc/executor/ISortModule.thrift
@@ -8,5 +8,10 @@ include "../ISource.thrift"
 
 service ISortModule{
 	void localCustomSort(1: ISource.ISource funct, 2: bool ascending) throws (1:IRemoteException.IRemoteException ex),
-	void localSort(1: bool ascending) throws (1:IRemoteException.IRemoteException ex);
+	void localSort(1: bool ascending) throws (1:IRemoteException.IRemoteException ex),
+	void sampling(1: i64 sampleSize, 2: i64 idx, 3: string master) throws (1:IRemoteException.IRemoteException ex),
+	void getPivots() throws (1:IRemoteException.IRemoteException ex),
+	void findPivots(1: list<string> nodes) throws (1:IRemoteException.IRemoteException ex),
+	void exchangePartitions(1: i64 idx, 2: list<string> nodes) throws (1:IRemoteException.IRemoteException ex),
+	void mergePartitions() throws (1:IRemoteException.IRemoteException ex);
 }
