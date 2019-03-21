b520651b533a65913282ec31ed4582f13ef20a29
César Pomar <cesarpomar18@gmail.com>
2019-03-21 18:52:48 +0100

    cross language variables

--- ignis/rpc/ISource.thrift
+++ ignis/rpc/ISource.thrift
@@ -2,7 +2,9 @@ namespace cpp ignis.rpc
 namespace java org.ignis.rpc
 namespace py ignis.rpc.source
 
-union ISource{
+
+struct ISource{
 	1: string name,
-	2: binary bytes;
+	2: binary bytes,
+	3: map <string, binary> _args;
 }
