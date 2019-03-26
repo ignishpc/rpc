b2ad694134295455bddded921ba0caa51714b93e
César Pomar <cesarpomar18@gmail.com>
2019-03-26 20:58:21 +0100

    ISource with union

--- ignis/rpc/ISource.thrift
+++ ignis/rpc/ISource.thrift
@@ -2,9 +2,12 @@ namespace cpp ignis.rpc
 namespace java org.ignis.rpc
 namespace py ignis.rpc.source
 
+union IEncoded {
+	1: string name,
+	2: binary bytes;
+}
 
 struct ISource{
-	1: string name,
-	2: binary bytes,
-	3: map <string, binary> _args = {};
+	1: required IEncoded obj,
+	2: map <string, binary> params = {};
 }
