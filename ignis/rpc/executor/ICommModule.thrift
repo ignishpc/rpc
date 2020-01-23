bbb109b7b288693fd7d9530a4246740a3a24884f
César Pomar <cesarpomar18@gmail.com>
2020-01-23 01:45:38 +0100

    ICommModule update

--- ignis/rpc/executor/ICommModule.thrift
+++ ignis/rpc/executor/ICommModule.thrift
@@ -10,7 +10,7 @@ service ICommModule{
 
 	string createGroup() throws (1:IExecutorException.IExecutorException ex),
 
-	void joinGroupMembers(1: string group, 2: i64 size) throws (1:IExecutorException.IExecutorException ex),
+	void joinGroupMembers(1: string group, 2: i64 id, 3: i64 size) throws (1:IExecutorException.IExecutorException ex),
 
 	void joinToGroup(1: string group, 2: string id) throws (1:IExecutorException.IExecutorException ex),
 
@@ -23,10 +23,14 @@ service ICommModule{
 	list<binary> getPartitions() throws (1:IExecutorException.IExecutorException ex),
 
 	void setPartitions(1: list<binary> partitions) throws (1:IExecutorException.IExecutorException ex),
+	
+	void setPartitions2(1: list<binary> partitions, 2: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
 
-	void driverGather(1: string id) throws (1:IExecutorException.IExecutorException ex),
+	void driverGather(1: string id, 2: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
 
-	void driverGather0(1: string id) throws (1:IExecutorException.IExecutorException ex),
+	void driverGather0(1: string id, 2: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
 
-	void driverScatter(1: string id) throws (1:IExecutorException.IExecutorException ex);
+	void driverScatter(1: string id, 2: i64 dataId) throws (1:IExecutorException.IExecutorException ex),
+
+	void driverScatter3(1: string id, 2: i64 dataId, 3: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex);
 }
