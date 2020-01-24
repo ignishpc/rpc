8213da1e12f4bb47f8c596635cf0154f66fee185
César Pomar <cesarpomar18@gmail.com>
2020-01-24 13:56:05 +0100

    rmProperty

--- ignis/rpc/driver/IPropertiesService.thrift
+++ ignis/rpc/driver/IPropertiesService.thrift
@@ -10,6 +10,7 @@ service IPropertiesService{
 	i64 newInstance2(1: i64 id) throws (1:IDriverException.IDriverException ex),
 	string setProperty(1: i64 id, 2: string key, 3: string value) throws (1:IDriverException.IDriverException ex),
 	string getProperty(1: i64 id, 2: string key) throws (1:IDriverException.IDriverException ex),
+	string rmProperty(1: i64 id, 2: string key) throws (1:IDriverException.IDriverException ex),
 	bool contains(1: i64 id, 2: string key) throws (1:IDriverException.IDriverException ex),
 	map<string, string> toMap(1: i64 id, 2: bool defaults) throws (1:IDriverException.IDriverException ex),
 	void fromMap(1: i64 id, 2:map<string, string> _map) throws (1:IDriverException.IDriverException ex),
