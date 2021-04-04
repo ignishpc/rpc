4829cfb2d34d065d34d5b983b1239c51364333dd
César Pomar <cesarpomar18@gmail.com>
2021-04-05 00:51:04 +0200

    CommModule new implementation

--- ignis/rpc/executor/ICommModule.thrift
+++ ignis/rpc/executor/ICommModule.thrift
@@ -8,15 +8,17 @@ include "../ISource.thrift"
 
 service ICommModule{
 
-	string createGroup() throws (1:IExecutorException.IExecutorException ex),
+	string openGroup() throws (1:IExecutorException.IExecutorException ex),
 
-	void joinGroupMembers(1: string group, 2: i64 id, 3: i64 size) throws (1:IExecutorException.IExecutorException ex),
+	void closeGroup() throws (1:IExecutorException.IExecutorException ex),
 
-	void joinToGroup(1: string group, 2: string id) throws (1:IExecutorException.IExecutorException ex),
+	void joinToGroup(1: string id, 2: bool leader) throws (1:IExecutorException.IExecutorException ex),
 
-	bool hasGroup(1: string id) throws (1:IExecutorException.IExecutorException ex),
+	void joinToGroupName(1: string id, 2: bool leader, 3: string name) throws (1:IExecutorException.IExecutorException ex),
 
-	void destroyGroup(1: string id) throws (1:IExecutorException.IExecutorException ex),
+	bool hasGroup(1: string name) throws (1:IExecutorException.IExecutorException ex),
+
+	void destroyGroup(1: string name) throws (1:IExecutorException.IExecutorException ex),
 
 	void destroyGroups() throws (1:IExecutorException.IExecutorException ex),
 
@@ -30,17 +32,21 @@ service ICommModule{
 	
 	void setPartitions2(1: list<binary> partitions, 2: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
 
-	void driverGather(1: string id, 2: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
+	void newEmptyPartitions(1: i64 n) throws (1:IExecutorException.IExecutorException ex),
 
-	void driverGather0(1: string id, 2: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
+	void newEmptyPartitions2(1: i64 n, 5: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
 
-	void driverScatter(1: string id, 2: i64 partitions) throws (1:IExecutorException.IExecutorException ex),
+	void driverGather(1: string group, 2: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
 
-	void driverScatter3(1: string id, 2: i64 partitions, 3: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
+	void driverGather0(1: string group, 2: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
 
-	void send(1: string id, 2: i64 partition, 3: i64 dest, 4: i64 tag)  throws (1:IExecutorException.IExecutorException ex),
+	void driverScatter(1: string group, 2: i64 partitions) throws (1:IExecutorException.IExecutorException ex),
 
-	void recv(1: string id, 2: i64 partition, 3: i64 source, 4: i64 tag)  throws (1:IExecutorException.IExecutorException ex),
+	void driverScatter3(1: string group, 2: i64 partitions, 3: ISource.ISource src) throws (1:IExecutorException.IExecutorException ex),
 
-	void recv5(1: string id, 2: i64 partition, 3: i64 source, 4: i64 tag, 5: ISource.ISource src)  throws (1:IExecutorException.IExecutorException ex);
+	i32 enableMultithreading(1: string group) throws (1:IExecutorException.IExecutorException ex),
+
+	void send(1: string group, 2: i64 partition, 3: i64 dest, 4: i32 thread)  throws (1:IExecutorException.IExecutorException ex),
+
+	void recv(1: string group, 2: i64 partition, 3: i64 source, 4: i32 thread)  throws (1:IExecutorException.IExecutorException ex);
 }
